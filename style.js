fd9da65226d2239f04c7181162ab0b675cdd2729
Etornam.Grives <grivesetornam@gmail.com>
2024-02-07 18:04:26 +0000

    mobile view

--- style.js
+++ style.js
@@ -1,4 +1,12 @@
 document.getElementById("messbtn").addEventListener("click",function(){
+    const input1 = document.getElementById("name").value.trim()
+    const input2 = document.getElementById("email").value.trim()
+    const input3 = document.getElementById("numberss").value.trim()
+    const input4 = document.getElementById("message").value.trim()
+
+
+    if(!input1 || !input2 || !input3 || !input4) return
+
     document.getElementsByClassName("pop-up")[0].classList.add("active");
 });
 
